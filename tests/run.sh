2c32f5c9a5b2ff9261e9ec8dc59107fcf3f4201a
sebalix <seb@usr-src.org>
2017-10-18 12:18:17 +0200

    [IMP] Drop support for Ansible < 2.4

--- tests/run.sh
+++ tests/run.sh
@@ -14,13 +14,13 @@ echo "== CHECK THE ROLE/PLAYBOOK'S SYNTAX =="
 ansible-playbook -i tests/inventory tests/test_$ODOO_INSTALL_TYPE.yml --syntax-check || exit 1
 
 echo "== RUN THE ROLE/PLAYBOOK WITH ANSIBLE-PLAYBOOK =="
-ansible-playbook -i tests/inventory tests/test_$ODOO_INSTALL_TYPE.yml --connection=local --sudo -e "odoo_version=$ODOO_VERSION" || exit 1
+ansible-playbook -i tests/inventory tests/test_$ODOO_INSTALL_TYPE.yml --connection=local --become -e "odoo_version=$ODOO_VERSION" || exit 1
 echo "== CHECK THE SERVICE STATUS =="
 sudo -E service odoo-$ODOO_INSTALL_TYPE status || exit 1
 
 echo "== RUN THE ROLE/PLAYBOOK AGAIN, CHECKING TO MAKE SURE IT'S IDEMPOTENT =="
 output_log=$ODOO_VERSION_$ODOO_INSTALL_TYPE.log
-ansible-playbook -i tests/inventory tests/test_${ODOO_INSTALL_TYPE}.yml --connection=local --sudo -e "odoo_version=$ODOO_VERSION" -v > $output_log || exit 1
+ansible-playbook -i tests/inventory tests/test_${ODOO_INSTALL_TYPE}.yml --connection=local --become -e "odoo_version=$ODOO_VERSION" -v > $output_log || exit 1
 grep -q 'changed=0.*failed=0' $output_log \
     && (echo 'IDEMPOTENCE TEST: OK' && exit 0) \
     || (echo 'IDEMPOTENCE TEST: FAILED' && cat $output_log && exit 1) || exit 1
@@ -28,7 +28,7 @@ echo "== CHECK THE SERVICE STATUS =="
 sudo -E service odoo-$ODOO_INSTALL_TYPE status || exit 1
 
 echo "== RUN THE ROLE/PLAYBOOK AGAIN BUT CHANGE THE CONFIGURATION AND CHECK IF THE SERVICE RESTART =="
-ansible-playbook -i tests/inventory tests/test_${ODOO_INSTALL_TYPE}_changed.yml --connection=local --sudo -e "odoo_version=$ODOO_VERSION" -v > $output_log || exit 1
+ansible-playbook -i tests/inventory tests/test_${ODOO_INSTALL_TYPE}_changed.yml --connection=local --become -e "odoo_version=$ODOO_VERSION" -v > $output_log || exit 1
 grep -q 'changed=2.*failed=0' $output_log \
     && (echo 'RESTART TEST: OK' && exit 0) \
     || (echo 'RESTART TEST: FAILED' && cat $output_log && exit 1) || exit 1
